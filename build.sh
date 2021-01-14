4fbae1f832528bf0711cb8c4a485bacf18fffa63
navidmostafiz <navidmostafiz@gmail.com>
2021-01-14 12:25:40 -0500

    src to app

--- build.sh
+++ build.sh
@@ -14,27 +14,27 @@ rm -rf dist;
 mkdir dist
 
 echo "*** compress termx.js";
-terser -c -m -- ./src/termx.js > ./dist/termx.js;
+terser -c -m -- ./app/termx.js > ./dist/termx.js;
 checkFile "./dist/termx.js";
 
 echo "*** compress termxc.js";
-terser -c -m -- ./src/termxc.js > ./dist/termxc.js;
+terser -c -m -- ./app/termxc.js > ./dist/termxc.js;
 checkFile "./dist/termxc.js";
 
 echo "*** compress termx.css";
-#### cleancss -o termx.css ./src/termx.css;
-uglifycss ./src/termx.css > ./dist/termx.css;
+#### cleancss -o termx.css ./app/termx.css;
+uglifycss ./app/termx.css > ./dist/termx.css;
 checkFile "./dist/termx.css";
 
 echo "*** compress termx.html";
-cp ./src/termx.html ./src/markup/termx.html;
-html-minifier-terser --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --input-dir ./src/markup/ --output-dir ./dist -o termx.html;
+cp ./app/termx.html ./app/markup/termx.html;
+html-minifier-terser --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --input-dir ./app/markup/ --output-dir ./dist -o termx.html;
 checkFile "./dist/termx.html";
 
 echo "*** copying mp3 files";
-cp ./src/*.mp3 ./dist/
+cp ./app/*.mp3 ./dist/
 checkFile "./dist/ping.mp3";
 
 echo "*** copying favicon";
-cp ./src/favicon.ico ./dist/
+cp ./app/favicon.ico ./dist/
 checkFile "./dist/favicon.ico";
